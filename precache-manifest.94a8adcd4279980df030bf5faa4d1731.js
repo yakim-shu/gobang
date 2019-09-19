3076855d75472e5b3dc9f56b603a0afccbb70c26
yakim shu <yakim-shu@users.noreply.github.com>
2019-09-19 14:30:52 +0800

    Init

---
+++ precache-manifest.94a8adcd4279980df030bf5faa4d1731.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66329bdebd84f8b338e072281837f3e",
+    "url": "/gobang/index.html"
+  },
+  {
+    "revision": "6f98ae2ec729e744c4fa",
+    "url": "/gobang/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "beef3485fb3af248df16",
+    "url": "/gobang/static/css/main.92036a1e.chunk.css"
+  },
+  {
+    "revision": "6f98ae2ec729e744c4fa",
+    "url": "/gobang/static/js/2.0c92a4e7.chunk.js"
+  },
+  {
+    "revision": "beef3485fb3af248df16",
+    "url": "/gobang/static/js/main.d579c925.chunk.js"
+  },
+  {
+    "revision": "68042749cab16fdc352a",
+    "url": "/gobang/static/js/runtime~main.89a96a38.js"
+  },
+  {
+    "revision": "a08a0cc8b56aec41f405347bac412198",
+    "url": "/gobang/static/media/ghost.a08a0cc8.png"
+  },
+  {
+    "revision": "d3927dc76d4ea6d9d4279cb6a160af4d",
+    "url": "/gobang/static/media/pacman.d3927dc7.png"
+  }
+]);
